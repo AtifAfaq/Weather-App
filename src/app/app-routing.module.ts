8a57b5ff6fb2a64ac579e8fcdac26034de46c422
Afaq, Muhammad <muhammad.afaq@accenture.com>
2020-04-24 12:50:56 -0400

    weather app dashboard and detail components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { DetailComponent } from './detail/detail.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: DashboardComponent },
+  { path: 'detail', component: DetailComponent },
+
+  { path: '', redirectTo: '/home', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
