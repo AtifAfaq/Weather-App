bd54d92561286e9cc72193d1e5182632748268ed
Afaq, Muhammad <muhammad.afaq@accenture.com>
2020-04-25 13:10:34 -0400

    calling for 3 cities

--- src/app/http-helper.service.ts
+++ src/app/http-helper.service.ts
@@ -5,13 +5,17 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root'
 })
 export class HttpHelperService {
+  cityIDs = '5128638,1689973,4887398';
+  appKey = '3d0fbf4365efb0de9cbe8fcbb4442519';
+  apiPath = 'http://api.openweathermap.org/data/2.5/group?';
 
   constructor(public http: HttpClient) {
   }
 
 
   getWeather() {
-    return this.http.get('http://api.openweathermap.org/data/2.5/group?id=5128638&appid=3d0fbf4365efb0de9cbe8fcbb4442519').toPromise();
+    var url = '${this.apiPath}id=${this.cityIDs}&appid=${appKey}'
+    return this.http.get(url).toPromise();
   }
 
 
